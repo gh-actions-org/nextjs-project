aa7f192f3e4807bebfa13b77817283c5b7fd2b2e
Vitor Sousa <vitor@Vitors-MacBook-Pro.local>
2024-09-08 08:33:31 +1000

    Added CI workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./e2e",
-  reporter: [["html", { open: "never" }]],
+  reporter: [
+    ["html", { open: "never" }],
+    ["json", { outputFile: "test-results.json" }],
+  ],
   webServer: {
     command: "npm run start",
     url: "http://localhost:3000",
