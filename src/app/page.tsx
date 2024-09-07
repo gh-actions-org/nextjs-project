c35c7d8c922ebcbaf725cdee8ddea8c023a9fccb
Vitor Sousa <vitor@Vitors-MacBook-Pro.local>
2024-09-08 08:38:26 +1000

    updated home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main className="main">
       <div>
-        <h2>Hello World</h2>
+        <h2>Hello World updated</h2>
       </div>
       <Link href="/about">About</Link>
       {flags.isFeatureEnabled("search") && (
